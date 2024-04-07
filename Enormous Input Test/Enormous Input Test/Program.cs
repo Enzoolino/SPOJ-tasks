63333f005d93d950eb3d6cbd51d299a32d961711
Krzysztof Wrona <31781455+Enzoolino@users.noreply.github.com>
2024-04-07 18:14:14 +0200

    Added Enormous Input Test task

---
+++ Enormous Input Test/Enormous Input Test/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        string[] inputNumbers = input.Split(' ');
+        
+        int n = int.Parse(inputNumbers[0]);
+        int k = int.Parse(inputNumbers[1]);
+
+        int[] integers = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            integers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int divCounter = 0;
+
+        foreach (var integer in integers)
+        {
+            if (integer % k == 0)
+            {
+                divCounter++;
+            }
+        }
+        
+        Console.WriteLine(divCounter);
+
+    }
+}
