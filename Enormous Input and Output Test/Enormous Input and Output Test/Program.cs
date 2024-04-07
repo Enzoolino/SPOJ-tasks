7d88e77301bf45f3b051a1bd79f32d982297da57
Krzysztof Wrona <31781455+Enzoolino@users.noreply.github.com>
2024-04-07 18:13:31 +0200

    Added Enormous Input and Output task

---
+++ Enormous Input and Output Test/Enormous Input and Output Test/Program.cs
@@ -0,0 +1,21 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        
+        int[,] integers = new int[n, 2];
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] inputNumebrs = Console.ReadLine().Split(' ');
+            integers[i, 0] = int.Parse(inputNumebrs[0]);
+            integers[i, 1] = int.Parse(inputNumebrs[1]);
+        }
+
+        for (int j = 0; j < n; j++)
+        {
+            Console.WriteLine(integers[j, 0] * integers[j, 1]);
+        }
+    }
+}
